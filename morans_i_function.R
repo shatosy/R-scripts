0956a600ad113ede1fca3f62b69b33d5c90fe589
Stephen Hatosy <shatosy@users.noreply.github.com>
2015-08-27 13:14:18 -0700

    Update morans_i_function.R

--- morans_i_function.R
+++ morans_i_function.R
@@ -1,23 +1,32 @@
-# Creates mean Bray-Curtis similarity based on rarefied "Sample by species" table
-# Arguments: "data" is a data matrix with samples as rows and OTU, genes, or species as columns, 
-# "perms" is the number of permutations you want to run (Defaults to 999)
-
-print("Loading required package: vegan")
+library(ape)
 library(vegan)
-multi.rarefy<-function(data,perms=999){
-	# Initialize Bray-Curtis variable
-	bc.sum=0
-	for(i in 1:perms)
-	{
-		# Rarefy data table
-		r.data=rrarefy(data,min(rowSums(data)))
-		# Generate Bray-Curtis matrix for rarefied table
-		r.data.bc=as.matrix(1-(vegdist(r.data)))
-		# Sum new Bray-Curtis matrix to previous Bray-Curtis matrix
-		bc.sum=bc.sum+r.data.bc
-	}
 
-	#Average of the Bray-Curtis similarities
-	bc.mean=bc.sum/i
-	return(bc.mean)
+# Computes Moran's I coeffecicents for time distance classes
+# Argument x is a vector of dates
+# Argument y is a community matrix with samples as rows and taxa as columns
+# Window is the size of time distance binning 
+# Skip is the time distance to move the window
+# Returns a data frame with time distance class, Moran's I coefficient, and p-value. Caution, p-value doesn't mean much unless the number of 
+# observations are equal across time distance class, which more than likely they are not. 
+# One can then plot column 1 (time distance class) vs. column 2 (Moran's I coefficient)
+moran.analysis<-function(x,y,window,skip){
+	corr.array=data.frame(dist.class=numeric(),mor.coef=numeric(),mor.p=numeric())
+	# Generate Euclidean time distances 
+	x=c(dist(x))
+	# Generate comminity similarity (1 - Bray Curtis Distance)
+	y=c(1-vegdist(y))
+	time.dist=as.matrix(dist(x))
+	bot=min(time.dist)
+	top=max(time.dist)
+	bin=1+(top-bot-window)/skip
+	for(i in 1:bin)
+	{
+		time.dist.bin=(time.dist>bot+(i-1)*skip & time.dist<=(bot+(i-1)*skip+window))
+		moran.stat=Moran.I(y,time.dist.bin,scaled=T)
+		mor.coef=moran.stat$observed
+		mor.p=moran.stat$p.value
+		dist.class=i
+		corr.array[i,]=c(dist.class,mor.coef,mor.p)
+	}
+	return(corr.array)
 }
